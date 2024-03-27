665c872361b5d96e4a7e8277a0aa0cd33aa89afa
Teddy <yoo.ji.yg@gmail.com>
2024-03-27 14:13:21 +0100

    add images for projects

--- my-portfolio/src/sections/ProjectCard.jsx
+++ my-portfolio/src/sections/ProjectCard.jsx
@@ -1,11 +1,11 @@
 import { Tag } from "../components/Tag";
 import { Button } from "../components/Button";
 import { Image } from "../components/Image";
-import repoImages from "./repoImages.json"
+import repoImages from "./repoImages.json";
 
 export const ProjectCard = ({ repositories }) => {
   const filteredRepo = repositories
-    .filter((repo) => repo.name.includes("project"))
+    .filter((repos) => repos.name.includes("project"))
 
     .sort((a, b) => {
       const repoA = new Date(a.created_at);
@@ -18,36 +18,38 @@ export const ProjectCard = ({ repositories }) => {
 
   return (
     <div>
-      {filteredRepo.map((repo) => (
-        <div className="proj-info" key={repo.id}>
-          const repoImage = repoImages.find((img) => img.repoName === repo.name)
-          
-          <Image 
-          divClassName={"proj-image-section"}
-          elementClassName={"proj-image"}
+      {filteredRepo.map((repo) => {
+        const repoImage = repoImages.find((img) => img.repoName === repo.name);
 
-          />
-          <p>{repo.name}</p>
-          <p>{repo.description}</p>
+        return (
+          <div className="proj-info" key={repo.id}>
+            <Image
+              divClassName={"proj-image-section"}
+              elementClassName={"proj-image"}
+              link={repoImage?.imageUrl}
+              imageAltText={repo.name}
+            />
+            <p>{repo.name}</p>
+            <p>{repo.description}</p>
 
-          {repo.topics.map((topic) => (
-            <div key={topic}>
-              <Tag tag={topic} />
-            </div>
-          ))}
-
-          <Button
-            iconUrl={"/Icons/globe.svg"}
-            iconAlt={"globe"}
-            text={"Live demo"}
-          />
-          <Button 
-          iconUrl={"/Icons/github.svg"}
-          iconAlt={"octocat"}
-          text={"Live demo"}
-          />
-        </div>
-      ))}
+            {repo.topics.map((topic) => (
+              <div key={topic}>
+                <Tag tag={topic} />
+              </div>
+            ))}
+            <Button
+              iconUrl={"/Icons/globe.svg"}
+              iconAlt={"globe"}
+              text={"Live demo"}
+            />
+            <Button
+              iconUrl={"/Icons/github.svg"}
+              iconAlt={"octocat"}
+              text={"Live demo"}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 };
